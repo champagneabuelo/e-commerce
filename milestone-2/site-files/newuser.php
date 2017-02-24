82c533794b85489e0bc6c12bcd0e832c11324dbc
Jason Valenzuela <jev4zs@virginia.edu>
2017-02-24 16:56:31 -0500

    fixed mysql query

--- milestone-2/site-files/newuser.php
+++ milestone-2/site-files/newuser.php
@@ -7,22 +7,22 @@ $conn = openCon();
 $fn 	= $_POST['firstname'];
 $ln 	= $_POST['lastname'];
 $email  = $_POST['email'];
+$pw	= $_POST['password'];
 $addr 	= $_POST['address'];
 $city 	= $_POST['city'];
 $st	= $_POST['state'];
 $zip	= $_POST['zip'];
-$pw	= $_POST['password'];
-
 
 $saveuser = "INSERT INTO siteusers 
 		(firstName, lastName, email, password, address, city, state, zipcode)
 		VALUES
-		('$fn','$ln','$email','$addr','$city','$st','$zip','$pw')";
+		('$fn','$ln','$email','$pw','$addr','$city','$st','$zip')";
 
 $result = mysqli_query($conn, $saveuser);
 
 if ($result) {
-	echo "Save successful!";
+	//echo "Save successful!";
+	header("location: index.html");
 } else {
 	echo "Save failed.";
 }
