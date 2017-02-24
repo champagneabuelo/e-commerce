513c8735db2c327a44ea277695c0d4893fbc6f1c
Jason Valenzuela <jev4zs@virginia.edu>
2017-02-24 16:45:16 -0500

    signup page now saves information to DB, need to validate information next

--- milestone-2/site-files/newuser.php
+++ milestone-2/site-files/newuser.php
@@ -1,4 +1,30 @@
-<? php
+<?php
+include 'dbconnect.php';
 
+// connect to the ecomm database
+$conn = openCon();
+
+$fn 	= $_POST['firstname'];
+$ln 	= $_POST['lastname'];
+$email  = $_POST['email'];
+$addr 	= $_POST['address'];
+$city 	= $_POST['city'];
+$st	= $_POST['state'];
+$zip	= $_POST['zip'];
+$pw	= $_POST['password'];
+
+
+$saveuser = "INSERT INTO siteusers 
+		(firstName, lastName, email, password, address, city, state, zipcode)
+		VALUES
+		('$fn','$ln','$email','$addr','$city','$st','$zip','$pw')";
+
+$result = mysqli_query($conn, $saveuser);
+
+if ($result) {
+	echo "Save successful!";
+} else {
+	echo "Save failed.";
+}
 
 ?>
